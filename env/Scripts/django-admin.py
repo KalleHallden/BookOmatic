fe32062ae32fe93195a8aa8d35e30bfa666c2e2c
The-Cali-Turtles <elijah.littellsivley@portageps.org>
2020-03-23 18:26:38 -0400

    I'm confused

---
+++ env/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!C:\PROJECTS\github-projects\kalleHellen-openSource\BookOmatic\env\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
