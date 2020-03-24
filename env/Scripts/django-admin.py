a7b0fb7340c4d0ae0c9a82d15d69543d511d76d8
CyberNotes7 <elittell214@gmail.com>
2020-03-24 19:25:10 -0400

    deleted

--- env/Scripts/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!C:\PROJECTS\github-projects\kalleHellen-openSource\BookOmatic\env\Scripts\python.exe
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
