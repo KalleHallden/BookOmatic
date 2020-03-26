568f3ef21733008012a416d3c58dd4b037ea8d0b
zwsmzzki <mateusz.zawadzki@rail.bombardier.com>
2020-03-26 18:27:41 +0100

    Finally reverted

--- BookOmatic/urls.py
+++ BookOmatic/urls.py
@@ -1,5 +1,5 @@
+  
 """BookOmatic URL Configuration
-
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/3.0/topics/http/urls/
 Examples:
@@ -13,12 +13,9 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.conf.urls import url, include
 from django.contrib import admin
+from django.urls import path
 
 urlpatterns = [
-    url(r'', include('calendarapp.urls')),
-    url(r'^admin/', admin.site.urls),
-
+    path('admin/', admin.site.urls),
 ]
-
