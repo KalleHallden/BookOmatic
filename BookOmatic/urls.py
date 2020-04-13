a2d0e46adfbe1dd0f377f3c9eccd087da546c246
mateuszz0000 <33598510+mateuszz0000@users.noreply.github.com>
2020-04-13 23:02:30 +0200

    Missing importing path in urls.py

--- BookOmatic/urls.py
+++ BookOmatic/urls.py
@@ -14,6 +14,7 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.conf.urls import url, include
+from django.urls import path
 from django.contrib import admin
 
 
