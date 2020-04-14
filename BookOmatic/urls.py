7a0298e652156066c0546964c3448410550c1845
jsphthms <joe@weduet.co>
2020-04-14 15:32:29 -0700

    fetched upstream

--- BookOmatic/urls.py
+++ BookOmatic/urls.py
@@ -13,7 +13,7 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.conf.urls import url, include
+from django.conf.urls import url, include, path
 from django.contrib import admin
 
 
